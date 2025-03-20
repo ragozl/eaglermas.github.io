de2ef02939f5dbd6e561a8f39d2598fba7a4256e
ragz <183134565+ragozl@users.noreply.github.com>
2025-03-20 18:41:08 -0400

    Update webhooksender.js

--- webhooksender.js
+++ webhooksender.js
@@ -1,5 +1,5 @@
 function sendwebhook(jsmsgusername, jsmessagecontent) {
-	  let jsweburl = document.getElementById("jsurl1").value;
+	  let jsweburl = "https://media.guilded.gg/webhooks/48125564-674a-427a-bbc8-e7036034dd7a/Cw0oCORbxuMQAwKoeQ4mk4i0oIwIyMwiwew0UuoIWag2oYEeykekGmQc0SoQ4MyYA40KacEEu22EWM0y8s2624";
   
 	  const request = new XMLHttpRequest();
       request.open("POST", jsweburl);
